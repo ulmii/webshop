9f0ab91742dfb83d56b67f8846cf399dee93464a
Konrad Ulman <veradopl@gmail.com>
2021-06-07 16:45:59 +0200

    Secure basket

--- play/app/controllers/BasketController.scala
+++ play/app/controllers/BasketController.scala
@@ -18,6 +18,7 @@ class BasketController @Inject()(
   extends CustomAbstractController[Basket] {
 
   override def create(): Action[JsValue] = SecuredAction(WithProvider[DefaultEnv#A](CredentialsProvider.ID)).async(scc.parsers.json)(implicit request => {
+
     request.body.validate[Basket].fold(
       _ => Future.successful(BadRequest("Cannot parse request")),
       basket => {
@@ -33,6 +34,19 @@ class BasketController @Inject()(
         }
       }
     )
+  })
+
+  def findOne(): Action[AnyContent] = SecuredAction(WithProvider[DefaultEnv#A](CredentialsProvider.ID)).async(implicit request => {
+
+    userService.retrieve(request.identity.loginInfo).flatMap {
+      case Some(_) =>
+        for {
+          result <- repository.findOne(request.identity.id.get).map(basket => Ok(Json.toJson(basket)))
+        } yield {
+          result
+        }
+      case None => Future.successful(BadRequest(JsString("Could not find user")))
+    }
   })
 
 }
